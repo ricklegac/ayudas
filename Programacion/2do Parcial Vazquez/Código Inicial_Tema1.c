100dcbf267cb9a0715a65e8b4703fcc6d7940444
ricklegac <rick.legac@gmail.com>
2021-10-31 22:31:05 -0300

    segunda parcial programacion

---
+++ Programacion/2do Parcial Vazquez/Código Inicial_Tema1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*Función principal solicitada*/
+void fmtMiles(int N, int A, char C, char sal[]){
+
+}
+
+int main(){
+
+    return 0;
+}
