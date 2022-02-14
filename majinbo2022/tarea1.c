66babde827edda58f185ee086af1c4508250909d
ricklegac <rick.legac@gmail.com>
2022-02-14 12:08:17 -0300

    push automatico, tiempo: 2022-02-14 12:08:17 -03

--- majinbo2022/tarea1.c
+++ majinbo2022/tarea1.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main(){
-
-
+	int n;
+	printf("ingrese el salario bruto del trabajador: \n");
+	scanf("%d",&n);
+	int ivatotal= n * 0.1;
+	int iva_retenido = ivatotal * 0.35;
+	int iva = ivatotal - iva_retenido;
+	printf("\nSALARIO BRUTO: %d\nIVA: %d\n IVA Retenido: %d\n SALARIO NETO: %d\n", n, iva, iva_retenido, n-ivatotal);
 	return 0;
 }
