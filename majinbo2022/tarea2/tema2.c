2a615fc8b269a16731b7f6654e05b56c28394e1c
ricklegac <rick.legac@gmail.com>
2022-02-23 11:57:24 -0300

    push automatico, tiempo: 2022-02-23 11:57:24 -03

--- majinbo2022/tarea2/tema2.c
+++ majinbo2022/tarea2/tema2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
 	printf("ingrese un numero de dos digitos: ");
 	int n;
@@ -11,23 +12,21 @@ int main(){
 	int unidad = n % 10;
 	printf("decena: %d\n", decena );
 	printf("unidad: %d\n", unidad );
-	return 0;
 	char cad_num[30]="";
 	switch (decena){
 		case 1:
-			cad_num = "diez y ";
+			strcat(cad_num, "dieci");
 			break;
 		case 2:
-			cad_num = "veinti";
+			strcat(cad_num, "veinti");
 			break;
 		case 3:
-			cad_num = "treinta y ";
+			strcat(cad_num, "treinta y");
 			break;
 		case 4:
-			cad_num= "cuarenta y";
+			strcat(cad_num, "cuarenta y");
 			break;
-
-
-	printf("%s", cad_num);
 	}
+	printf("%s", cad_num);
+	return 0;
 }
