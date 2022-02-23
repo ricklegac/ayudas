49442328648734e841c1dbd3fae694e7d30bdd1f
ricklegac <rick.legac@gmail.com>
2022-02-23 11:32:19 -0300

    push automatico, tiempo: 2022-02-23 11:32:19 -03

--- majinbo2022/tarea2/tema1.c
+++ majinbo2022/tarea2/tema1.c
@@ -5,10 +5,22 @@ int main(){
 	scanf("%f",&n);
 	float prestamo;
 	if(n<0){
-		n = -n + n + 10000;
 		prestamo = -n + 10000;
-		printf("prestamo otorgado: %.2f",prestamo);
+		n = -n + n + 10000;
+		
+	}else if(n>=0 && n<20000){
+		prestamo = 20000-n;
+		n= prestamo + n;
+	}else{
+		prestamo = 0;
 	}
-
+	printf("prestamo otorgado: %.2f\n",prestamo);
+	printf("capital actual: %.2f\n", n);
+	printf("----------------------------------------\n");
+	printf("p/ equipo de computo: 5000\n");
+	printf("p/ equipo mobiliario: 2000\n");
+	printf("p/ compras de insumos: %2.f\n",n/2);
+	printf("p/ otorgar incentivos al personal: %2.f\n", n/2);
+	printf("----------------------------------------\n");
 	return 0;
 }
