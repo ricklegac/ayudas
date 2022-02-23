88655aa8096640a5ec93d668cf3a17b4968fb7df
ricklegac <rick.legac@gmail.com>
2022-02-23 11:48:42 -0300

    push automatico, tiempo: 2022-02-23 11:48:42 -03

--- majinbo2022/tarea2/tema2.c
+++ majinbo2022/tarea2/tema2.c
@@ -2,12 +2,14 @@
 int main(){
 	printf("ingrese un numero de dos digitos: ");
 	int n;
-	scanf("%d",n);
+	scanf("%d",&n);
 	while(n<10 || n>99){
 		printf("numero no valido, vuelva a ingresar: ");
-		scanf("%d",n);
+		scanf("%d",&n);
 	}
-	decena = n % 10;
-	printf("decena: %d", decena );
+	int decena = n / 10;
+	int unidad = n % 10;
+	printf("decena: %d\n", decena );
+	printf("unidad: %d\n", unidad );
 	return 0;
 }
