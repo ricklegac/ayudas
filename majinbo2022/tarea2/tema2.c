f4276eda143634063afa6dde5dc67554b2d5697a
ricklegac <rick.legac@gmail.com>
2022-02-23 12:04:44 -0300

    push automatico, tiempo: 2022-02-23 12:04:44 -03

--- majinbo2022/tarea2/tema2.c
+++ majinbo2022/tarea2/tema2.c
@@ -37,36 +37,66 @@ int main(){
 		}
 	}else{
 		switch (decena){
-		case 1:
-			strcat(cad_num, "dieci");
-			break;
-		case 2:
-			strcat(cad_num, "veinti");
-			break;
-		case 3:
-			strcat(cad_num, "treinta y");
-			break;
-		case 4:
-			strcat(cad_num, "cuarenta y");
-			break;
-		case 5:
-			strcat(cad_num, "cuarenta y");
-			break;
-		case 6:
-			strcat(cad_num, "cuarenta y");
-			break;
-		case 7:
-			strcat(cad_num, "cuarenta y");
-			break;
-		case 8:
-			strcat(cad_num, "cuarenta y");
-			break;
-		case 9:
-			strcat(cad_num, "cuarenta y");
-			break;
+			case 1:
+				strcat(cad_num, "dieci");
+				break;
+			case 2:
+				strcat(cad_num, "veinti");
+				break;
+			case 3:
+				strcat(cad_num, "treinta y");
+				break;
+			case 4:
+				strcat(cad_num, "cuarenta y");
+				break;
+			case 5:
+				strcat(cad_num, "cincuenta y");
+				break;
+			case 6:
+				strcat(cad_num, "sesenta y");
+				break;
+			case 7:
+				strcat(cad_num, "setenta y");
+				break;
+			case 8:
+				strcat(cad_num, "ochenta y");
+				break;
+			case 9:
+				strcat(cad_num, "noventa y");
+				break;
+			}
+		switch(unidad){
+			case 1:
+				strcat(cad_num, "uno");
+				break;
+			case 2:
+				strcat(cad_num, "dos");
+				break;
+			case 3:
+				strcat(cad_num, "tres");
+				break;
+			case 4:
+				strcat(cad_num, "cuatro");
+				break;
+			case 5:
+				strcat(cad_num, "cinco");
+				break;
+			case 6:
+				strcat(cad_num, "seis");
+				break;
+			case 7:
+				strcat(cad_num, "siete");
+				break;
+			case 8:
+				strcat(cad_num, "ocho");
+				break;
+			case 9:
+				strcat(cad_num, "nueve");
+				break;
+		}
 	}
-	}
-	
-	printf("%s", cad_num);
+	printf("\n-------------------------------");
+	printf("\n%s\n", cad_num);
+	printf("-------------------------------");
 	return 0;
 }
